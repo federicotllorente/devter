c2cd25ffb36a4ead07d2358c4ca0ae46d8052f9e
Federico Tejedor Llorente <federicotllorente@gmail.com>
2021-05-12 13:32:24 +0200

    Starting to fetch data to the Next server

--- pages/timeline/index.js
+++ pages/timeline/index.js
@@ -1,5 +1,15 @@
-export default function Timeline() {
-    return (
-        <h1>This is the timeline</h1>
-    )
+export default function Timeline({ username }) {
+	return (
+		<h1>This is the timeline of {username}</h1>
+	)
+}
+
+Timeline.getInitialProps = () => {
+	const location = 'http://localhost:3000'
+	return fetch(`${location}/api/hello`)
+		.then(res => res.json())
+		.then(res => {
+			const { username } = res
+			return { username }
+		})
 }
